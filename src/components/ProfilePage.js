4966ea4f797de570258fcf35e8907248c5687abc
Newton Manyisa <manyisanewton26@gmail.com>
2025-01-23 00:28:05 +0300

    add

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./ProfilePage.css";
-// Importing images for the profile
-import ProfileBackground from "../assets/profilebackground.jpeg";
 
-import MyProfileImage from "/home/newton/social-media-app/src/components/assets/profile.jpeg";
-import AliceProfile from "/home/newton/social-media-app/src/components/assets/post6.jpeg";
-import BobProfile from "./assets/Bobprofile.jpg"; // Retains Bob's profile
+// Importing images for the profile
+import ProfileBackground from "./assets/profilebackground.jpeg";
+import MyProfileImage from "./assets/profile.jpeg";
+import AliceProfile from "./assets/post6.jpeg";
+import BobProfile from "./assets/Bobprofile.jpg";
 
 // Importing images for the previous posts
-import Post1 from "/home/newton/social-media-app/src/components/assets/post1.jpeg";
-import Post2 from "/home/newton/social-media-app/src/components/assets/post2.jpeg";
-import Post3 from "/home/newton/social-media-app/src/components/assets/post3.jpeg";
-import Post4 from "/home/newton/social-media-app/src/components/assets/post4.jpeg";
-import Post5 from "/home/newton/social-media-app/src/components/assets/post5.jpeg";
-import Post6 from "/home/newton/social-media-app/src/components/assets/post6.jpeg";
+import Post1 from "./assets/post1.jpeg";
+import Post2 from "./assets/post2.jpeg";
+import Post3 from "./assets/post3.jpeg";
+import Post4 from "./assets/post4.jpeg";
+import Post5 from "./assets/post5.jpeg";
+import Post6 from "./assets/post6.jpeg";
 
 function ProfilePage() {
   return (
@@ -21,7 +21,6 @@ function ProfilePage() {
       <div className="profileTop">
         <div className="profileCover">
           <img src={ProfileBackground} alt="Cover" className="profileCoverImg" />
-          {/* Avatar photo */}
           <img src={MyProfileImage} alt="Avatar" className="profileUserImg" />
         </div>
         <div className="profileInfo">
@@ -34,36 +33,25 @@ function ProfilePage() {
         <div className="profileBottomLeft">
           <h5 className="profileSectionTitle">About</h5>
           <p className="profileAboutText">
-          Hello! I'm Sandra Misigo, a dedicated developer who enjoys building innovative social media platforms and inspiring others through knowledge sharing.
+            Hello! I'm Sandra Misigo, a dedicated developer who enjoys building
+            innovative social media platforms and inspiring others through knowledge sharing.
           </p>
         </div>
         <div className="profileBottomRight">
           <h5 className="profileSectionTitle">My Friends</h5>
           <div className="profileFriends">
             <div className="profileFriend">
-              {/* Alice's profile picture */}
-              <img
-                src={AliceProfile}
-                alt="Friend"
-                className="profileFriendImg"
-              />
+              <img src={AliceProfile} alt="Friend" className="profileFriendImg" />
               <span className="profileFriendName">Ruth</span>
             </div>
             <div className="profileFriend">
-              {/* Bob's profile picture */}
-              <img
-                src={BobProfile}
-                alt="Friend"
-                className="profileFriendImg"
-              />
+              <img src={BobProfile} alt="Friend" className="profileFriendImg" />
               <span className="profileFriendName">Alex</span>
             </div>
-            {/* Add more friends as needed */}
           </div>
         </div>
       </div>
 
-      {/* Previous Posts Section */}
       <div className="profilePosts">
         <h5 className="profileSectionTitle">Sandra's Posts</h5>
         <div className="postGrid">
